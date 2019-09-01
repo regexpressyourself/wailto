1655a8f6c82293a48487c65fe561ee298440caea
Sam <samuel.messina@siu.edu>
2019-09-01 17:17:00 -0500

    adding build files
    my poor server cant build the files itself

---
+++ front/build/precache-manifest.5ed6697523f2abe55bc1a85d9efad1ed.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d166f08bf8612066af1a2acdfcc71a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b7d74c205566490e373",
+    "url": "/static/css/2.bb342caa.chunk.css"
+  },
+  {
+    "revision": "ecbc60eab1d1ad8377d9",
+    "url": "/static/css/main.bc73c5dc.chunk.css"
+  },
+  {
+    "revision": "4b7d74c205566490e373",
+    "url": "/static/js/2.5269df4b.chunk.js"
+  },
+  {
+    "revision": "ecbc60eab1d1ad8377d9",
+    "url": "/static/js/main.aebb1322.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "02a1c71d258f1d2659bf01b1c5fa2206",
+    "url": "/static/media/friday.02a1c71d.jpg"
+  },
+  {
+    "revision": "2a3978c54fe18ed08e47aa5303ca4e15",
+    "url": "/static/media/sm.2a3978c5.png"
+  },
+  {
+    "revision": "4450519cd28c6bbdb71405a225b9dba3",
+    "url": "/static/media/wailto-logo.4450519c.png"
+  }
+]);

337b79a296566ffab4df651681f74ae242b8ac78
Sam <samuel.messina@siu.edu>
2019-10-06 10:46:31 -0500

    revert hour

--- front/src/components/modules/SongsByHour.js
+++ front/src/components/modules/SongsByHour.js
@@ -1,13 +1,44 @@
 import React, {useContext, useState, useEffect} from 'react';
-import Graph from './Graph';
-import {hourToAmpm} from '../../functions/dateMappers';
+import './charts.scss';
+import {SongHistoryContext} from '../../context/SongHistoryContext';
+import {ConfigContext} from '../../context/ConfigContext';
+import {ResponsiveContainer, AreaChart, Area, XAxis, YAxis, Tooltip} from 'recharts';
+import {hourToAmpm, bucketSongTimes} from '../../functions/dateMappers';
+import {getGenreKey, getGenre2Key} from '../../functions/genres';
 
 const SongsByHour = () => {
-  const dataKey = 'hour';
-  let hourNames = [];
-  for (let i = 0; i < 24; i++) {
-    hourNames.push(hourToAmpm(i));
-  }
+  const {songHistory} = useContext(SongHistoryContext);
+  const {config} = useContext(ConfigContext);
+
+  let [hourDataRC, setHourDataRC] = useState(null);
+  let [initialKey, setInitialKey] = useState(getGenreKey(config.genre, config.genre2));
+  let [secondaryKey, setSecondaryKey] = useState( getGenre2Key(config.genre, config.genre2));
+
+  useEffect(() => {
+    let map = {};
+    let tempInitialKey = getGenreKey(config.genre, config.genre2);
+    let tempSecondaryKey = getGenre2Key(config.genre, config.genre2);
+    setInitialKey(tempInitialKey);
+    setSecondaryKey(tempSecondaryKey);
+
+    map[tempInitialKey] = bucketSongTimes('hour', 24, songHistory.songHistory, config.genre);
+    if (config.genre2) {
+      map[tempSecondaryKey] = bucketSongTimes('hour', 24, songHistory.songHistory, config.genre2);
+    }
+    let tempHourDataRC = [];
+    for (let i = 0; i <= 23; i++) {
+      let newHourObject = {
+        name: hourToAmpm(i),
+      };
+
+      newHourObject[tempInitialKey] = map[tempInitialKey][i] ? map[tempInitialKey][i].length : 0;
+      newHourObject[tempSecondaryKey] =
+        map[tempSecondaryKey] && map[tempSecondaryKey][i] ? map[tempSecondaryKey][i].length : 0;
+
+      tempHourDataRC.push(newHourObject);
+    }
+    setHourDataRC(tempHourDataRC);
+  }, [songHistory.songHistory, config.genre, config.genre2]);
 
   return (
     <>
@@ -17,7 +48,17 @@ const SongsByHour = () => {
           <br /> <span className="per">&mdash;by&mdash;</span> <br />
           Hour of Day
         </h1>
-        <Graph dataKey={dataKey} dataKeyValues={hourNames} />
+        <ResponsiveContainer>
+          <AreaChart data={hourDataRC}>
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Area type="monotone" dataKey={initialKey} stroke="#7f4782" fill="#aa5c9f" />
+            {secondaryKey ? (
+              <Area type="monotone" dataKey={secondaryKey} stroke="#fd8b7b" fill="#e2598b" />
+            ) : null}
+          </AreaChart>
+        </ResponsiveContainer>
       </div>
     </>
   );

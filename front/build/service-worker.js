de89c5eff2ccc26a56e2a4612da318316bd3f258
Sam <samuel.messina@siu.edu>
2019-10-26 11:20:44 -0500

    add build

--- front/build/service-worker.js
+++ front/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7512aedd9931c0c1412a176ce4f5407a.js"
+  "/precache-manifest.c840ce504d9ec86bf5ffeaee03cadb03.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
